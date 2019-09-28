d2a3c9130dd3bfa864f7e1dba413a0f7570e8497
James Woolfenden <james.woolfenden@gmail.com>
2019-09-28 08:34:18 +0100

    Update and rename aws_iam_role_confluent_ssm_role.tf to aws_iam_role.confluent_ssm_role.tf

--- aws_iam_role.confluent_ssm_role.tf
+++ aws_iam_role.confluent_ssm_role.tf
@@ -23,13 +23,12 @@ resource "aws_iam_role" "confluent_ssm_role" {
 EOF
 }
 
-
 variable "roles" {
   type = list
   default = [
     "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM",
     "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
-  "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"]
+    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"]
 }
 
 resource "aws_iam_role_policy_attachment" "role-attach" {

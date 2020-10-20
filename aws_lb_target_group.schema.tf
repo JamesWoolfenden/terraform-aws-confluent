c58176c100248d731ba542dee65983586ab27d0c
James Woolfenden <James.Woolfenden@gmail.com>
2020-10-20 07:37:58 +0100

    fix tags

---
+++ aws_lb_target_group.schema.tf
@@ -0,0 +1,13 @@
+resource "aws_lb_target_group" "schema" {
+  name     = "SCHEMA"
+  port     = 6668
+  protocol = "TCP"
+  vpc_id   = data.aws_vpc.confluent.id
+
+  stickiness {
+    enabled = false
+    type    = "lb_cookie"
+  }
+
+  tags = var.common_tags
+}

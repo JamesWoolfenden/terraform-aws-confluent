530b51018b53cb3c4a1b2815cf5426406aac32b1
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 20:12:04 +0100

    fix: replace deprecated aws_subnet_ids with aws_subnets

--- example/example-real/data.network.tf
+++ example/example-real/data.network.tf
@@ -4,38 +4,37 @@ data "aws_vpcs" "vpc" {
     values = [upper(var.account_name)]
   }
 }
-
 locals {
   vpc_id = tolist(data.aws_vpcs.vpc.ids)[0]
 }
-
-
 data "aws_vpc" "vpc" {
   id = local.vpc_id
 }
-
-data "aws_subnet_ids" "private" {
-  vpc_id = local.vpc_id
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [local.vpc_id]
+  }
 
   tags = {
     Type = "Private"
   }
 }
-
-data "aws_subnet_ids" "public" {
-  vpc_id = local.vpc_id
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [local.vpc_id]
+  }
 
   tags = {
     Type = "Public"
   }
 }
-
 data "aws_subnet" "public" {
-  count = length(tolist(data.aws_subnet_ids.public.ids))
-  id    = tolist(data.aws_subnet_ids.public.ids)[count.index]
+  count = length(tolist(data.aws_subnets.public.ids))
+  id    = tolist(data.aws_subnets.public.ids)[count.index]
 }
-
 data "aws_subnet" "private" {
-  count = length(tolist(data.aws_subnet_ids.private.ids))
-  id    = tolist(data.aws_subnet_ids.private.ids)[count.index]
+  count = length(tolist(data.aws_subnets.private.ids))
+  id    = tolist(data.aws_subnets.private.ids)[count.index]
 }
